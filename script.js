1bd5f58595439e770f3b6526ebee2b93620b3a5f
YunJi Park <pyg5087@gmail.com>
2023-05-07 15:45:26 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,95 @@
+// let a = 10;
+// const b = 2;
+// let myName = "YunJi";
+// console.log(a+b);
+// console.log(a/b);
+// console.log(a*b);
+
+// console.log("hello " + myName)
+
+// myName= "nicolas";
+// console.log("your new name is" + myName)
+
+// const amIFat= null;
+// let something;
+// console.log(something);
+// const mon = "mon";
+// const tue = "tue";
+// const wed = "wed";
+// const thu = "thu";
+// const fri = "fri";
+// const sat = "sat";
+// const sun = "sun";
+
+// const daysOfWeek = mon + tue + wed + thu + fri + sat + sun
+// console.log(daysOfWeek);
+// 단지 스트링일 뿐임
+
+// const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+// console.log(daysOfWeek);
+// console.log(daysOfWeek[2]); //번호 불러오기, 컴퓨터는 0부터 카운트
+// daysOfWeek.push("sun"); 
+// //항목 하나를 array 안에 추가
+
+// const player1 = {
+//     name: "nico",
+//     points: 10,
+//     fat: true,
+// };
+
+// console.log(player1) ;
+// console.log(player1.name);
+// //console.log(player.name); = console.log(player["name"]);
+// player1.name
+
+// //function: 내가 계속 반복해서 사용할 수 있는 코드 조각(실행을 여러 번 할 수 있게 해줌)
+// //function은 대괄호와 중괄호 같이 사용
+// function sayHello(){
+//     //내가 여기 작성하는 것이, sayHello를 실행할 때마다 실행됨
+// }
+// function sayHello(){
+//     console.log("Hello!");
+// }
+
+// sayHello();
+// sayHello();
+// sayHello(); //function 실행을 3번함
+// //argument(인수): function을 싱행하는 동안 어떤 정보를 function에게 보낼 수 있는 방법
+
+// function sayHello(nameOfPerson, age){
+//     console.log("Hello my name is" + nameOfPerson + "and I'm"+ age);
+// }
+// sayHello("nico",10)
+// sayHello("yunji",23)
+// sayHello("sinian", 34) //자바스크립트는 sayHello로 보내진 첫 번째 데이터가 nameOfPerson이라는 variable로 가게됨
+
+// function plus(a, b){
+//     console.log(a, b);
+// }
+// plus() //undefined
+
+// function plus(a,b){
+//     console.log(a, b);
+// }
+// plus() //NaN(not a number)
+
+// function plus(firstNumber, secondNumber){
+//     console.log(firstNumber + secondNumber);
+// }
+// plus(60, 8);
+// //firstNumer= 60, secondNumber=8
+
+// const player = {
+//     name: "nico",
+//     sayHello: function(otherPersonsName){
+//         console.log("hello! " + otherPersonsName + " nice to meet you");
+//     },
+// };
+// console.log(player.name)
+//  //"박윤지"
+// player.sayHello("lynn"); //sayHello는 어떠한 argument도 받지 않음
+
+
+
+
+
